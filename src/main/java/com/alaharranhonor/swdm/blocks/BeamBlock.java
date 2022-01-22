b751a04a657290661c616ce0fd6a24a6b28b61bd
Silverwolf11420 <treefrog11420@gmail.com>
2022-01-21 19:25:02 -0600

    Little fix

--- src/main/java/com/alaharranhonor/swdm/blocks/BeamBlock.java
+++ src/main/java/com/alaharranhonor/swdm/blocks/BeamBlock.java
@@ -25,56 +25,62 @@ public class BeamBlock extends Block {
 
     @Override
     public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, IWorld pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
-        if (pFacing == Direction.UP) {
-            if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.SINGLE) {
-                    return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.BOTTOM);
-            }
-            if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.UPPER) {
-                return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.MIDDLE);
-            }
-            if (pFacingState.isAir()) {
-                if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.BOTTOM) {
-                    return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.SINGLE);
+            if (pFacing == Direction.UP) {
+                if (pFacingState.getBlock() instanceof BeamBlock) {
+                    if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.SINGLE && pFacingState.getValue(TILE) == SWDMBlockstateProperties.Tileable.UPPER) {
+                        return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.BOTTOM);
+                    }
+                    if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.UPPER && pFacingState.getValue(TILE) == SWDMBlockstateProperties.Tileable.UPPER) {
+                        return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.MIDDLE);
+                    }
                 }
-                if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.MIDDLE) {
-                    return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.UPPER);
-                }
-            }
-        }
-        //come back to
-       else if (pFacing == Direction.DOWN) {
-            if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.BOTTOM) {
-                return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.MIDDLE);
-            }
-            if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.SINGLE) {
-                return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.UPPER);
-            }
-            if (pFacingState.isAir()) {
-                if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.MIDDLE) {
-                    return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.BOTTOM);
-                }
-                if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.UPPER) {
-                    return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.SINGLE);
+                if (pFacingState.isAir()) {
+                    if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.BOTTOM) {
+                        return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.SINGLE);
+                    }
+                    if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.MIDDLE) {
+                        return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.UPPER);
+                    }
                 }
             }
-        }
+            else if (pFacing == Direction.DOWN) {
+                if (pFacingState.getBlock() instanceof BeamBlock) {
+                    if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.BOTTOM && pFacingState.getValue(TILE) == SWDMBlockstateProperties.Tileable.BOTTOM) {
+                        return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.MIDDLE);
+                    }
+                    if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.SINGLE && pFacingState.getValue(TILE) == SWDMBlockstateProperties.Tileable.SINGLE) {
+                        return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.UPPER);
+                    }
+                }
+                if (pFacingState.isAir()) {
+                    if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.MIDDLE) {
+                        return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.BOTTOM);
+                    }
+                    if (pState.getValue(TILE) == SWDMBlockstateProperties.Tileable.UPPER) {
+                        return pState.setValue(TILE, SWDMBlockstateProperties.Tileable.SINGLE);
+                    }
+                }
+            }
         return super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
     }
 
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext pContext) {
-
+        if (pContext.getPlayer().isCrouching()) {
+            return super.getStateForPlacement(pContext);
+        }
         BlockState state = pContext.getLevel().getBlockState(pContext.getClickedPos().below());
         BlockState state1 = pContext.getLevel().getBlockState(pContext.getClickedPos().above());
+
         if (state.getBlock() instanceof BeamBlock && state1.getBlock() instanceof BeamBlock) {
-            return state.getBlock().defaultBlockState().setValue(TILE, SWDMBlockstateProperties.Tileable.MIDDLE);
+            return this.defaultBlockState().setValue(TILE, SWDMBlockstateProperties.Tileable.MIDDLE);
         }
         else if (state.getBlock() instanceof BeamBlock) {
-            return state.getBlock().defaultBlockState().setValue(TILE, SWDMBlockstateProperties.Tileable.UPPER);
+            return this.defaultBlockState().setValue(TILE, SWDMBlockstateProperties.Tileable.UPPER);
         }
         else if (state1.getBlock() instanceof BeamBlock) {
-            return state1.getBlock().defaultBlockState().setValue(TILE, SWDMBlockstateProperties.Tileable.BOTTOM);
+            return this.defaultBlockState().setValue(TILE, SWDMBlockstateProperties.Tileable.BOTTOM);
         }
         return super.getStateForPlacement(pContext);
     }
