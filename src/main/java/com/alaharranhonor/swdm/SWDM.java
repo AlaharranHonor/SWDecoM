c29cf08e2f14892a8c23442eba3fee38b2717fbd
Silverwolf11420 <treefrog11420@gmail.com>
2022-02-18 14:57:16 -0600

    Fixed meter points

--- src/main/java/com/alaharranhonor/swdm/SWDM.java
+++ src/main/java/com/alaharranhonor/swdm/SWDM.java
@@ -3,12 +3,15 @@ package com.alaharranhonor.swdm;
 import com.alaharranhonor.swdm.util.init.BlockInit;
 import com.alaharranhonor.swdm.util.init.SWDMPaintings;
 import com.alaharranhonor.swdm.util.init.SWDMTileEntities;
+import com.alaharranhonor.swdm.util.init.SWEMInit;
+import com.alaharranhonor.swem.util.registry.SWEMBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ComposterBlock;
 import net.minecraft.block.WoodType;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShovelItem;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
@@ -72,6 +75,18 @@ public class SWDM
             ComposterBlock.COMPOSTABLES.put(BlockInit.SPRUCE_LEAVES_SLAB_ITEM.get(), 0.3F);
             ComposterBlock.COMPOSTABLES.put(BlockInit.SPRUCE_LEAVES_STAIRS_ITEM.get(), 0.3F);
             ComposterBlock.COMPOSTABLES.put(BlockInit.SPRUCE_LEAVES_WALL_ITEM.get(), 0.3F);
+
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_BLACK.get(), SWEMInit.METER_POINT_BLACK.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_BLUE_GRAY.get(), SWEMInit.METER_POINT_BLUE_GRAY.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_BROWN.get(), SWEMInit.METER_POINT_BROWN.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_DARK_BROWN.get(), SWEMInit.METER_POINT_DARK_BROWN.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_DUSTED_GRAY.get(), SWEMInit.METER_POINT_DUSTED_GRAY.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_GOLDEN.get(), SWEMInit.METER_POINT_GOLDEN.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_GRAY.get(), SWEMInit.METER_POINT_GRAY.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_LIGHT_GRAY.get(), SWEMInit.METER_POINT_LIGHT_GRAY.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_MUTED_BROWN.get(), SWEMInit.METER_POINT_MUTED_BROWN.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_VIVID_RED.get(), SWEMInit.METER_POINT_VIVID_RED.defaultBlockState());
+            ShovelItem.FLATTENABLES.put(BlockInit.SANDSTONE_WHITE.get(), SWEMInit.METER_POINT_WHITE.defaultBlockState());
         });
     }
 
