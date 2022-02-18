08389423edcdc615130919e7313761ae0cd67999
Silverwolf11420 <treefrog11420@gmail.com>
2022-02-18 13:58:33 -0600

    Added whitewash mirrors

--- src/main/java/com/alaharranhonor/swdm/util/init/SWEMInit.java
+++ src/main/java/com/alaharranhonor/swdm/util/init/SWEMInit.java
@@ -5,6 +5,7 @@ import com.alaharranhonor.swdm.blocks.BeamBlock;
 import com.alaharranhonor.swlm.SWLM;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.PaintingType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.ToolType;
@@ -33,6 +34,14 @@ public class SWEMInit {
     public static Block WHITEWASH_LOG_WALL;
     public static Block WHITEWASH_PLANK_BEAM;
     public static Block WHITEWASH_LOG_BEAM;
+    public static PaintingType MIRROR_WHITEWASH_16x16;
+    public static PaintingType MIRROR_WHITEWASH_16x32;
+    public static PaintingType MIRROR_WHITEWASH_32x16;
+    public static PaintingType MIRROR_WHITEWASH_32x32;
+    public static PaintingType MIRROR_WHITEWASH_64x32;
+    public static PaintingType MIRROR_WHITEWASH_64x48;
+    public static PaintingType MIRROR_WHITEWASH_64x64;
+
 
     /* Blocks are intialized first.
      * Store the reference to the block, and then in the item initialization register the items and block items.
@@ -75,6 +84,26 @@ public class SWEMInit {
             WHITEWASH_LOG_BEAM = new BeamBlock(AbstractBlock.Properties.copy(Blocks.ACACIA_PLANKS)).setRegistryName("whitewash_log_beam");
             item.getRegistry().register(WHITEWASH_LOG_BEAM);
 
+        }
+    }
+
+    @SubscribeEvent
+    public static void intializeModPaintings(RegistryEvent.Register<PaintingType> item) {
+        if (ModList.get().isLoaded("swem")) {
+            MIRROR_WHITEWASH_16x16 = new PaintingType(16, 16).setRegistryName("mirror_whitewash_16x16");
+            item.getRegistry().register(MIRROR_WHITEWASH_16x16);
+            MIRROR_WHITEWASH_16x32 = new PaintingType(16, 32).setRegistryName("mirror_whitewash_16x32");
+            item.getRegistry().register(MIRROR_WHITEWASH_16x32);
+            MIRROR_WHITEWASH_32x16 = new PaintingType(32, 16).setRegistryName("mirror_whitewash_32x16");
+            item.getRegistry().register(MIRROR_WHITEWASH_32x16);
+            MIRROR_WHITEWASH_32x32 = new PaintingType(32, 32).setRegistryName("mirror_whitewash_32x32");
+            item.getRegistry().register(MIRROR_WHITEWASH_32x32);
+            MIRROR_WHITEWASH_64x32 = new PaintingType(64, 32).setRegistryName("mirror_whitewash_64x32");
+            item.getRegistry().register(MIRROR_WHITEWASH_64x32);
+            MIRROR_WHITEWASH_64x48 = new PaintingType(64, 48).setRegistryName("mirror_whitewash_64x48");
+            item.getRegistry().register(MIRROR_WHITEWASH_64x48);
+            MIRROR_WHITEWASH_64x64 = new PaintingType(64, 64).setRegistryName("mirror_whitewash_64x64");
+            item.getRegistry().register(MIRROR_WHITEWASH_64x64);
         }
     }
 
